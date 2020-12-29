d54e2f3428b447681bd9c20c248f4ffd1a6fb5b4
Brandon Weiss <brandonweiss2008@gmail.com>
2020-12-29 13:24:59 -0500

    updated .env file

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8001', // 'https://shrouded-depths-39976.herokuapp.com',
-  TOKEN_KEY: 'taxhub-client-auth-token',
+  API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT, // 'https://shrouded-depths-39976.herokuapp.com',
+  TOKEN_KEY: process.env.REACT_APP_TOKEN_KEY
 }
