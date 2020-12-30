cb60be8740a89ac4f2505feeae7998aa3ad7cb53
Brandon Weiss <brandonweiss2008@gmail.com>
2020-12-30 13:31:28 -0500

    changed api endpoint

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || 'http://localhost:8001',
+  API_ENDPOINT: 'https://polar-citadel-03770.herokuapp.com',
   TOKEN_KEY: process.env.REACT_APP_TOKEN_KEY
 }
