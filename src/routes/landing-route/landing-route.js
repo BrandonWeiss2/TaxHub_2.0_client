7c3403e441f623a271a67989c33da057aaf6d12f
Brandon Weiss <brandonweiss2008@gmail.com>
2020-12-30 14:07:53 -0500

    added landing route

--- src/routes/landing-route/landing-route.js
+++ src/routes/landing-route/landing-route.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import Logo from '../components/images/logo.png'
+import Logo from '../images/logo.png'
 import './landing-route.css'
 
 export default class LandingRoute extends Component {
